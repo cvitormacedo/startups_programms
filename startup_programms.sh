c5a07322e1c36b30bd398a58049b1fdbceafc178
vitor <vitor.costamacedo1@gmail.com>
2022-04-01 01:02:56 -0300

    adding themes

--- startup_programms.sh
+++ startup_programms.sh
@@ -2,6 +2,8 @@
 
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
+sudo apt-get install gnome-tweaks -y
+sudo apt install zip unzip
 
 sudo apt update -y
 
@@ -25,6 +27,8 @@ sudo apt update -y
 
 ## Installing external programms ##
 
+mkdir /home/$UESR/.themes
+mkdir /home/$UESR/.icons
 mkdir /home/$USER/Downloads/programms
 cd /home/$USER/Downloads/programms
 
@@ -34,6 +38,18 @@ sudo dpkg -i *.deb
 sudo apt update -y
 chmod +x Anaconda3-2021.11-Linux-x86_64.sh
 ./Anaconda3-2021.11-Linux-x86_64.sh
+
+cd
+
+mkdir /home/$USER/Downloads/personalisation
+cd /home/$USER/Downloads/personalisation
+
+wget -c https://dl2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTY0ODE2MDE1OCwidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiIzYTU1MDAxN2Y2ZDQwZGI1MmMyYTg4NGFhNDkzMmMwZmExZTc2YzU1NGJkODkwOWQ4MzM4ZWM3NzhjYzY4ZjhlOGQ5NzE2MWQ5OTZjNzU1MzAzYmRkMzMyNjJhODZmNjI2M2FhNDc1ZWJlMDk3ODQ5YTViZGQ3Y2M5NzkwNGM4YiIsInQiOjE2NDg3ODcwMTUsInN0ZnAiOiI2MzI5ODM5OGNiMGY4Y2ZjY2Y1NTA0YTFmNmYxYjFkNSIsInN0aXAiOiIyODA0OmQ1MTo2MDkzOmEzMDA6Y2M0MjphMjhlOjc4NDc6NmU1MiJ9.XYcVT0ndkz56ek4WWPw78ljAS2Gr665gdcNP0FpjVg0/Juno-ocean-v40.tar.xz
+wget -c https://codeload.github.com/m4thewz/dracula-icons/zip/refs/heads/main
+
+tar -xvf Juno-ocean-v40.tar.xz
+unzip dracula-icons-main.zip
+
 
 ## Installing repository programms ##
 
